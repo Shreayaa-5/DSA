2861519172d0f1bcb43801d00c009e46649e2665
Shreayaa Srinivasan <159439019+Shreayaa-5@users.noreply.github.com>
2025-06-23 22:24:32 -0900

    Time: 8 ms (4.18%) | Memory: 45 MB (8.46%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,24 +12,58 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy=new ListNode(0);
-        ListNode curr=dummy;
-        int carry=0;
-        while (l1!=null ||l2!=null || carry!=0){
-            int val1=(l1!=null)?l1.val:0;
-            int val2=(l2!=null)?l2.val:0;
+        int[] arr1 = new int[100];
+        int[] arr2 = new int[100];
 
-            int sum=val1+val2+carry;
-            int digit=sum%10;
-            carry=sum/10;
-
-            curr.next=new ListNode(digit);
-            curr=curr.next;
-
-            if(l1!=null) l1=l1.next;
-            if(l2!=null) l2=l2.next;
-
+        ListNode temp = l1;
+        int k = 0;
+        while (temp != null) {
+            arr1[k++] = temp.val;
+            temp = temp.next;
         }
-        return dummy.next;
+
+        ListNode temp1 = l2;
+        int j = 0;
+        while (temp1 != null) {
+            arr2[j++] = temp1.val;
+            temp1 = temp1.next;
+        }
+
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        for (int i = k - 1; i >= 0; i--) {
+            s1.append(arr1[i]);
+        }
+        for (int i = j - 1; i >= 0; i--) {
+            s2.append(arr2[i]);
+        }
+
+        BigInteger num1 = new BigInteger(s1.toString());
+        BigInteger num2 = new BigInteger(s2.toString());
+        BigInteger res = num1.add(num2);
+
+        // Build result list
+        if (res.equals(BigInteger.ZERO)) return new ListNode(0);
+
+        ListNode head = new ListNode(0); // dummy head
+
+        while (res.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger[] divmod = res.divideAndRemainder(BigInteger.TEN);
+            int digit = divmod[1].intValue();
+            res = divmod[0];
+
+            // Create new node
+            ListNode newnode = new ListNode(digit);
+
+            // Traverse from head to the end using curr.next
+            ListNode curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newnode;
+        }
+
+        return head.next;
     }
 }

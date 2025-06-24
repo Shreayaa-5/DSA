032f3b46863e7cc97c7200a2333bbdaef0a10d9a
Shreayaa Srinivasan <159439019+Shreayaa-5@users.noreply.github.com>
2025-06-24 07:24:05 -0900

    Time: 0 ms (100.00%) | Memory: 43.4 MB (10.34%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,14 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long nn=Math.abs((long)n);
+        double result=1.0;
+        while(nn>=1){
+            if(nn%2==1){
+                result*=x;
+            }
+            x*=x;
+            nn/=2;
+        }
+        return (n<0) ? 1/result:result;
     }
 }

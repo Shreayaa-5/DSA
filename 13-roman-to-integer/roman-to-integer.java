b1a5061698eedca68d779704eea7fd8cb2712aea
Shreayaa Srinivasan <159439019+Shreayaa-5@users.noreply.github.com>
2025-07-27 17:44:29 +0530

    Time: 6 ms (20.37%) | Memory: 45 MB (34.66%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,21 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<String,Integer> hh=new HashMap<>();
-        hh.put("I",1);
-        hh.put("V",5);
-        hh.put("X",10);
-        hh.put("L",50);
-        hh.put("C",100);
-        hh.put("D",500);
-        hh.put("M",1000);
+        HashMap<Character,Integer> hh=new HashMap<>();
+        hh.put('I',1);
+        hh.put('V',5);
+        hh.put('X',10);
+        hh.put('L',50);
+        hh.put('C',100);
+        hh.put('D',500);
+        hh.put('M',1000);
         int ans=0;
         for(int i=0;i<s.length()-1;i++){
-            if(hh.get(s.charAt(i)+"")<hh.get(s.charAt(i+1)+"")){
-                ans-=hh.get(s.charAt(i)+"");
+            if(hh.get(s.charAt(i))<hh.get(s.charAt(i+1))){
+                ans-=hh.get(s.charAt(i));
             }else{
-                ans+=hh.get(s.charAt(i)+"");
+                ans+=hh.get(s.charAt(i));
             }
         }
-        return ans+=hh.get(s.charAt(s.length()-1)+"");
+        return ans+=hh.get(s.charAt(s.length()-1));
     }
 }

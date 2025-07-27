f4e2e443920f81569c5b7a800c8cafade317af46
Shreayaa Srinivasan <159439019+Shreayaa-5@users.noreply.github.com>
2025-07-27 17:42:42 +0530

    Time: 9 ms (5.89%) | Memory: 46.2 MB (6.71%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -8,47 +8,14 @@ class Solution {
         hh.put("C",100);
         hh.put("D",500);
         hh.put("M",1000);
-        hh.put("IV",4);
-        hh.put("IX",9);
-        hh.put("XL",40);
-        hh.put("XC",90);
-        hh.put("CD",400);
-        hh.put("CM",900);
         int ans=0;
-        boolean end_pair=false;
         for(int i=0;i<s.length()-1;i++){
-            String a="";
-            if(s.charAt(i)=='I' && s.charAt(i+1)=='V'){
-                ans+=hh.get("IV");
-                if(i==s.length()-2) end_pair=true;
-                i++;
-            }else if(s.charAt(i)=='I' && s.charAt(i+1)=='X'){
-                ans+=hh.get("IX");
-                if(i==s.length()-2) end_pair=true;
-                i++;
-            }else if(s.charAt(i)=='X' && s.charAt(i+1)=='L'){
-                ans+=hh.get("XL");
-                if(i==s.length()-2) end_pair=true;
-                i++;
-            }else if(s.charAt(i)=='X' && s.charAt(i+1)=='C'){
-                ans+=hh.get("XC");
-                if(i==s.length()-2) end_pair=true;
-                i++;
-            }else if(s.charAt(i)=='C' && s.charAt(i+1)=='D'){
-                ans+=hh.get("CD");
-                if(i==s.length()-2) end_pair=true;
-                i++;
-            }else if(s.charAt(i)=='C' && s.charAt(i+1)=='M'){
-                ans+=hh.get("CM");
-                if(i==s.length()-2) end_pair=true;
-                i++;
+            if(hh.get(s.charAt(i)+"")<hh.get(s.charAt(i+1)+"")){
+                ans-=hh.get(s.charAt(i)+"");
             }else{
                 ans+=hh.get(s.charAt(i)+"");
-            }  
+            }
         }
-        if(!end_pair){
-            ans+=hh.get(s.charAt(s.length()-1)+"");
-        }
-        return ans;
+        return ans+=hh.get(s.charAt(s.length()-1)+"");
     }
 }
